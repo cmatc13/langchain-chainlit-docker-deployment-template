c416c7308fcda4d5994a8e3ba8c13a5ff19df8cd
Callum Matchett <cmatc13@yahoo.com>
2024-04-18 13:34:09 +0200

    Update main.py

--- demo_app/main.py
+++ demo_app/main.py
@@ -3,7 +3,31 @@ import sys
 import os
 sys.path.append(os.path.abspath('.'))
 
-from langchain import PromptTemplate, OpenAI, LLMChain
+import numpy as np
+#import pandas as pd
+import datetime as dt
+import os
+#import matplotlib.pyplot as plt
+from langchain.embeddings.openai import OpenAIEmbeddings
+#from langchain_openai import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import PromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate,  ChatPromptTemplate
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import EmbeddingsFilter
+from langchain.retrievers.self_query.base import SelfQueryRetriever
+from langchain.chains.query_constructor.base import AttributeInfo
+from langchain.vectorstores import FAISS, Chroma
+import csv
+from typing import Dict, List, Optional
+from langchain.document_loaders.base import BaseLoader
+from langchain.docstore.document import Document
+from langchain.chains.llm import LLMChain
+#from google.colab import userdata
+#import lark
+
+from langchain.chat_models import ChatOpenAI
 import chainlit as cl
 
 from chainlit import user_session
@@ -21,6 +45,6 @@ def factory():
     user_env = cl.user_session.get("env")
     os.environ["OPENAI_API_KEY"] = user_env.get("OPENAI_API_KEY")
     prompt = PromptTemplate(template=template, input_variables=["question"])
-    llm_chain = LLMChain(prompt=prompt, llm=OpenAI(temperature=0), verbose=True)
+    llm_chain = LLMChain(prompt=prompt, llm=ChatOpenAI(model_name="gpt-3.5-turbo-0125",temperature=0), verbose=True)
 
     return llm_chain
